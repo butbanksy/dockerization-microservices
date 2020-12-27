c3aaed1747339882d5a9604ac72210702bc25522
Mahmoud Rhazzoul <56742148+butbanksy@users.noreply.github.com>
2020-12-27 03:38:45 +0100

    Added dockercompose.yml
    Created the Users microservice.

---
+++ UsersMicroservice/Data/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersMicroservice.Models;
+
+namespace UsersMicroservice.Data
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        
+        public UserContext(DbContextOptions<UserContext> opt) : base(opt)
+        {
+            
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=postgres-0;Database=dockerization;Username=postgres;Password=postgres");
+    }
+}
